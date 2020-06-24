c1cd0daa554542dd7c824438044117979b7aed0d
fouad97 <loukrichi21@gmail.com>
2020-06-24 13:24:09 +0100

    broken homepage into smaller components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/directory-menu/directory";
+import "./homepage.scss";
+
+const Homeage = () => (
+	<div className="homepage">
+		<Directory />
+	</div>
+);
+
+export default Homeage;
