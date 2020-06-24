7d57730c05906b0e7a132a073aeffbae1dc2b127
fouad97 <loukrichi21@gmail.com>
2020-06-24 14:43:04 +0100

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import "./App.css";
+
+const hats = (props) => {
+	console.log(props);
+
+	return (
+		<div>
+			<button onClick={() => props.history.push("/")}>home</button>
+			<h1>Hats page</h1>;
+		</div>
+	);
+};
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<Link to="/">Home</Link>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route exact path="/hats" component={hats} />
+			</Switch>
 		</div>
 	);
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Directory from "../../components/directory-menu/directory";
 import "./homepage.scss";
 
-const Homeage = () => (
+const Homeage = (props) => (
 	<div className="homepage">
 		<Directory />
 	</div>
