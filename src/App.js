1dc0b9e7b4286ed04612beb6d7b57f5fd04899af
fouad97 <loukrichi21@gmail.com>
2020-06-24 18:08:17 +0100

    adding header components and links to home, shop and sign in pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import Shop from "./pages/shop/shop";
+import Header from "./components/header/header";
 import "./App.css";
 
 function App() {
 	return (
 		<div>
-			<Link to="/">Home</Link>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route exact path="/shop" component={Shop} />
+				<Route exact path="/contact" component={() => <h1>contact</h1>} />
+				<Route exact path="/signin" component={() => <h1>Sign in</h1>} />
 			</Switch>
 		</div>
 	);
