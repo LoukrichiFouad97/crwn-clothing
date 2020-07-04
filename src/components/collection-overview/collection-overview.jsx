ff5f12dd39bc7a76f7251be9b70ea9a73256521d
fouad97 <loukrichi21@gmail.com>
2020-07-04 16:34:47 +0100

    create respective selectors and updated components with redux flow with directory/shop

--- src/components/collection-overview/collection-overview.jsx
+++ src/components/collection-overview/collection-overview.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCollections } from "../../redux/shop/shop-selectors";
+import { selectCollectionForPreview } from "../../redux/shop/shop-selectors";
 import PreviewCollection from "../preview-collection/preview-collection";
 
-import "./collection-overview.scss";
+import "./collection-overview.scss"; 
 
 const CollectionsOverview = ({ collections }) => (
 	<div className="collection-overview">
-		{collections.map((collection) => (
+		{
+		collections.map((collection) => (
 			<PreviewCollection key={collection.id} {...collection} />
-		))}
+		))
+		}
 	</div>
 );
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectCollections,
+	collections: selectCollectionForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
